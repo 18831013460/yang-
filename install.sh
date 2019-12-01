77d3f5ec994d64100940d1091e0df1fa79506bab
18831013460 <18831013460>
2019-12-01 11:07:12 +0800

    hello

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ apt update
    #above vim8 and vim8 can use python3
 apt install git
 apt install w3m  build-essential gcc gcc-g++
+git clone https://github.com/VundleVim/Vundle.vim.git
 git clone https://github.com/ranger/ranger.git
 apt install python3 python3-dev ctags make automkake
 cp    .vimrc ~/.vimrc
