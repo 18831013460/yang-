4c6ff4c61e92bd0d1b1eef518f4381128dd3f483
18831013460 <18831013460>
2019-12-01 14:18:33 +0800

    final

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@ apt update
    #above vim8 and vim8 can use python3
 apt install git
 apt install w3m  build-essential gcc gcc-g++
-git clone https://github.com/VundleVim/Vundle.vim.git
 git clone https://github.com/ranger/ranger.git
 apt install python3 python3-dev ctags make automkake
 cp    .vimrc ~/.vimrc
 cp -r .vim/.  ~/.vim
-vim +PluginInstall +qall
 cd .vim/bundle/YouCompleteMe
 install.py --java-completer --clang-complter --clangd-completer
