89c6a0af61618d1285850797d2b5bffa72e12056
杨晓帆 <52170730+18831013460@users.noreply.github.com>
2020-03-02 15:22:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 apt update
-   #above vim8 and vim8 can use python3
+#above vim8 and vim8 can use python3
 apt install git
 apt install w3m  build-essential gcc gcc-g++
 git clone https://github.com/ranger/ranger.git
 apt install python3 python3-dev ctags make automkake
-cp    .vimrc ~/.vimrc
-cp -r .vim/.  ~/.vim
+touch .vimrc
+mkdir .vim
+cp    .vimrc ~/
+cp -r ~/.vim/.vim  ~/
 cd .vim/bundle/YouCompleteMe
 install.py --java-completer --clang-complter --clangd-completer
