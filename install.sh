3314db7720c6978663102a71f7f6066576c8462d
杨晓帆 <52170730+18831013460@users.noreply.github.com>
2020-03-02 16:17:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt install git vim
 apt install w3m  build-essential gcc gcc-g++
 git clone https://github.com/ranger/ranger.git
 apt install python3 python3-dev ctags make automkake cmake
-
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp    .vimrc ~/
 cp -r ~/.vim/.vim  ~/
 cd .vim/bundle/YouCompleteMe
